93eba69f81ca60f87990cd9dc8bb60dc3c014b01
Euclides Fernandes <ef.webdev@gmail.com>
2023-04-11 15:03:59 -0300

    Comment out unused alias

--- test/swapy/data_stream_test.exs
+++ test/swapy/data_stream_test.exs
@@ -1,7 +1,7 @@
 defmodule Swapy.DataStreamTest do
   use ExUnit.Case
 
-  alias Swapy.DataStream
+  # alias Swapy.DataStream
 
   describe "new/1" do
     test "should create a stream type of struct" do
